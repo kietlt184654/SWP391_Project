c7c94cb73322a1d1edfe9881412e625e97d01bae
kietltse184654 <kietlu021104@gmail.com>
2024-09-26 09:11:32 +0700

    Update Sprint1

--- src/main/java/com/example/swp391/config/SecurityConfig.java
+++ src/main/java/com/example/swp391/config/SecurityConfig.java
@@ -2,12 +2,17 @@ package com.example.swp391.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig   {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,13 +25,23 @@ public class SecurityConfig {
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")  // Trang đăng nhập tùy chỉnh
                         .defaultSuccessUrl("/home", true)  // Điều hướng đến /home sau khi đăng nhập thành công
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // Đường dẫn để logout
+                        .logoutSuccessUrl("/login?logout")  // Sau khi đăng xuất chuyển về trang login
+                        .invalidateHttpSession(true) // Hủy phiên làm việc (session)
+                        .deleteCookies("JSESSIONID") // Xóa cookie của phiên đăng nhập
+                        .permitAll()
+
                 );
 
         return http.build();
+
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
         return null; // Placeholder nếu bạn cần UserDetailsService riêng
     }
+
 }
