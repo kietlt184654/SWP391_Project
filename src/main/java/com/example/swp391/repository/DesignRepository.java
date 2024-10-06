3e82488393b5e0d38f712cc1c1014cafc5878129
An0208-meme <anttse183436@fpt.edu.vn>
2024-10-06 16:23:10 +0700

    annn

--- src/main/java/com/example/swp391/repository/DesignRepository.java
+++ src/main/java/com/example/swp391/repository/DesignRepository.java
@@ -1,4 +1,15 @@
 package com.example.swp391.repository;
 
-public interface DesignRepository {
+import com.example.swp391.entity.DesignEnity;
+import com.example.swp391.entity.TypeDesignEnity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DesignRepository extends JpaRepository<DesignEnity, Long> {
+    List<DesignEnity> findByDesignName(String designName);
+    List<DesignEnity> findByDesignNameContainingIgnoreCaseAndTypeDesign_TypeDesignId(String designName, Long typeDesignId);
 }
+
