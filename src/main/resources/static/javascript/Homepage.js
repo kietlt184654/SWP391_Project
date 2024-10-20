f0a5042af7b9e341ca803606616fd83234a07b4b
kietltse184654 <kietlu021104@gmail.com>
2024-10-20 07:31:39 +0700

    Edit Profile Update Mapping

--- src/main/resources/static/javascript/Homepage.js
+++ src/main/resources/static/javascript/Homepage.js
@@ -1,94 +1,32 @@
-// window.onload = function () {
-//   showSlides();
-// };
+window.onload = function () {
+    showSlides();
+};
 
-// var slideIndex = 0;
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
+var slideIndex = 0;
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
 
-//   // Hide all slides
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-
-//   slideIndex++;
-//   if (slideIndex > slides.length) {
-//     slideIndex = 1;
-//   }
-
-//   // Remove the "active" class from all dots
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-
-//   // Display the current slide and make the corresponding dot active
-//   slides[slideIndex - 1].style.display = "block";
-//   dots[slideIndex - 1].className += " active";
-
-//   // Automatically change slide every 2 seconds
-//   setTimeout(showSlides, 2000);
-// }
-// // 
-var nextBtn = document.querySelector('.next'),
-    prevBtn = document.querySelector('.prev'),
-    carousel = document.querySelector('.carousel'),
-    list = document.querySelector('.list'), 
-    item = document.querySelectorAll('.item'),
-    runningTime = document.querySelector('.carousel .timeRunning') 
-
-let timeRunning = 3000 
-let timeAutoNext = 7000
-
-nextBtn.onclick = function(){
-    showSlider('next')
-}
-
-prevBtn.onclick = function(){
-    showSlider('prev')
-}
-
-let runTimeOut 
-
-let runNextAuto = setTimeout(() => {
-    nextBtn.click()
-}, timeAutoNext)
-
-
-function resetTimeAnimation() {
-    runningTime.style.animation = 'none'
-    runningTime.offsetHeight /* trigger reflow */
-    runningTime.style.animation = null 
-    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
-}
-
-
-function showSlider(type) {
-    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
-    if(type === 'next'){
-        list.appendChild(sliderItemsDom[0])
-        carousel.classList.add('next')
-    } else{
-        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
-        carousel.classList.add('prev')
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
 
-    clearTimeout(runTimeOut)
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
 
-    runTimeOut = setTimeout( () => {
-        carousel.classList.remove('next')
-        carousel.classList.remove('prev')
-    }, timeRunning)
+    // Remove the "active" class from all dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
 
+    // Display the current slide and make the corresponding dot active
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 
-    clearTimeout(runNextAuto)
-    runNextAuto = setTimeout(() => {
-        nextBtn.click()
-    }, timeAutoNext)
-
-    resetTimeAnimation() // Reset the running time animation
+    // Automatically change slide every 2 seconds
+    setTimeout(showSlides, 2000);
 }
-
-// Start the initial animation 
-resetTimeAnimation()
