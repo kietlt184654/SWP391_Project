9876afb5c14d787c0d9e9e81823c2a215ab801ae
Nguyenqnhu <nnquynhnhu49@gmail.com>
2024-11-09 23:43:01 +0700

    profile

--- src/main/resources/static/javascript/profile.js
+++ src/main/resources/static/javascript/profile.js
@@ -8,18 +8,18 @@ menuToggle.addEventListener("click", () => {
 
 // dropdown menu
 const accountIcon = document.getElementById("accountIcon");
-const dropdownMenu = document.querySelector(".dropdown-menu");
+// const dropdownMenu = document.querySelector(".dropdown-menu");
 
-accountIcon.addEventListener("click", (event) => {
-  event.preventDefault();
-  dropdownMenu.style.display =
-    dropdownMenu.style.display === "block" ? "none" : "block";
-});
+// accountIcon.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   dropdownMenu.style.display =
+//     dropdownMenu.style.display === "block" ? "none" : "block";
+// });
 
 document.addEventListener("click", function (event) {
   if (
-    !accountIcon.contains(event.target) &&
-    !dropdownMenu.contains(event.target)
+    !accountIcon.contains(event.target)
+    // !dropdownMenu.contains(event.target)
   ) {
     dropdownMenu.style.display = "none";
   }
@@ -61,44 +61,3 @@ links.forEach((link) => {
     link.classList.add("highlighted");
   }
 });
-
-//  order tracking
-const circles = document.querySelectorAll(".circle"),
-  progressBar = document.querySelector(".indicator"),
-  buttons = document.querySelectorAll("button");
-
-let currentStep = 1;
-const updateSteps = (e) => {
-  currentStep = e.target.id === "next" ? ++currentStep : --currentStep;
-  circles.forEach((circle, index) => {
-    circle.classList[`${index < currentStep ? "add" : "remove"}`]("active");
-  });
-
-  progressBar.style.width = `${
-    ((currentStep - 1) / (circles.length - 1)) * 100
-  }%`;
-  if (currentStep === circles.length) {
-    buttons[1].disabled = true;
-  } else if (currentStep === 1) {
-    buttons[0].disabled = true;
-  } else {
-    buttons.forEach((button) => (button.disabled = false));
-  }
-};
-buttons.forEach((button) => {
-  button.addEventListener("click", updateSteps);
-});
-
-//show img profile
-document
-  .getElementById("avatar-upload")
-  .addEventListener("change", function (event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        document.getElementById("avatar-image").src = e.target.result;
-      };
-      reader.readAsDataURL(file);
-    }
-  });
