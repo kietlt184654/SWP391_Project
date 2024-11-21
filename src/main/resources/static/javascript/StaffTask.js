20b97efba2d0d03f0f05607cff7455904bc92220
giakien321 <kientgse182386@fpt.edu.vn>
2024-11-22 01:36:25 +0700

    Update UI of some page

--- src/main/resources/static/javascript/StaffTask.js
+++
@@ -1,4 +0,0 @@
-// Reload the page to update the status after form submission
-if (window.performance && window.performance.navigation.type === window.performance.navigation.TYPE_BACK_FORWARD) {
-    window.location.reload();
-}
